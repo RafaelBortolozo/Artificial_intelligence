307c61dbfef8d6380b63cac0f27083d7a6e3badf
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-04-01 23:57:23 -0300

    feat: search working

--- searchAStar.js
+++ searchAStar.js
@@ -1,6 +1,6 @@
-const initialCombination = [3,3,0,0]
-const finalCombination = [0,0,3,3]
-let root = null
+const initialCombination = [3,3,1,0,0,0] //missionarios_left, canibais_left, missionarios_right, canibais_right
+const finalCombination = [0,0,0,3,3,1]
+let combinationHistory = [initialCombination]
 
 const combinationsBoat = [
     [1,0],
@@ -10,16 +10,16 @@ const combinationsBoat = [
     [0,2]
 ]
 
-function createNode(combination, boatSide){
+function createNode(combination){
     return {
         combination,
-        boatSide,
         children: []
     }
 }
 
-function hammingDistance(init, end){
-    function convertArrayInBinaryString(array){ //transforma a combinação em array para uma string binaria, aumentando a precisão da distancia de Hamming
+function decimalDistance(init, end){
+    //transforma a combinação em array para uma string binaria
+    function convertArrayInBinaryString(array){ 
         let str = ''
         for(let i of array){
             for(let j=0 ; j<3 ; j++){
@@ -34,21 +34,30 @@ function hammingDistance(init, end){
         return str
     }
 
-    init = convertArrayInBinaryString(init).split('') //split na string para comparar caracteres
-    end = convertArrayInBinaryString(end).split('')
+    init = convertArrayInBinaryString(init)
+    end = convertArrayInBinaryString(end)
 
-    let count = 0
-    for(let i in end){ //contagem de caracteres diferentes
-        if(init[i] != end[i]) count++
-    }
-    
+    let count = parseInt(init, 2) + parseInt(end, 2)
+
     return count
 }
 
 function validatePossibility(possibility){
+    //verificar numero de missionarios e canibais (ignorando o lado do barco)
+    for(let value of possibility){
+        if(value < 0 || value > 3) return false
+    }
+
+    //verificar se a combinação já foi analisada
+    let analyzedCombination = combinationHistory.some(item => {
+        return item.toString() == possibility.toString()
+    })
+    if(analyzedCombination) return false
+    
     //verificar se há mais canibais que missionarios, a condicional é invalida caso não há missionarios naquele lado
-    if( (possibility[0] < possibility[1] && possibility[0] > 0) || 
-        (possibility[2] < possibility[3] && possibility[2] > 0)){ 
+    if(
+        (possibility[0] < possibility[1] && possibility[0] != 0) || 
+        (possibility[3] < possibility[4] && possibility[3] != 0)){ 
         return false
     }else{
         return true
@@ -59,23 +68,27 @@ function getValidPossibilities(node){
     let newValidPossibilities = [] //array de novas possibilidades validas
     let possibility = []
     for(let combinationBoat of combinationsBoat){ //para cada combinação de barco...
-        if (node.boatSide === 'left') { // se barco estar a esquerda, remove pessoas da esquerda e joga pra direita ou então o inverso
+        if (node.combination[2] == 1) { // se barco estar a esquerda, remove pessoas da esquerda e joga pra direita ou então o inverso
             possibility = [ 
                 node.combination[0] - combinationBoat[0],
                 node.combination[1] - combinationBoat[1],
-                node.combination[2] + combinationBoat[0],
-                node.combination[3] + combinationBoat[1]
+                0,
+                node.combination[3] + combinationBoat[0],
+                node.combination[4] + combinationBoat[1],
+                1
             ]
         } else {
             possibility = [ 
                 node.combination[0] + combinationBoat[0],
                 node.combination[1] + combinationBoat[1],
-                node.combination[2] - combinationBoat[0],
-                node.combination[3] - combinationBoat[1]
+                1,
+                node.combination[3] - combinationBoat[0],
+                node.combination[4] - combinationBoat[1],
+                0
             ]
         }
-        
-        if(validatePossibility(possibility)){ //verificar possibilidade
+        //verificar possibilidade
+        if(validatePossibility(possibility)){ 
             newValidPossibilities.push(possibility)
         } 
     }
@@ -83,27 +96,32 @@ function getValidPossibilities(node){
     return newValidPossibilities
 }
 
+function isDifferent(array1, array2){
+    if(array1.toString() != array2.toString()) return true
+
+    return false
+}
+
 function createSearchATree(rootState) {
-    if(rootState == null){ //inicialmente cria uma raiz da arvore, é o estado inicial do problema
-        rootState = createNode(initialCombination, 'left')
+    //inicialmente cria uma raiz da arvore, é o estado inicial do problema
+    if(rootState == null){ 
+        rootState = createNode(initialCombination)
     }
-    
-    if (rootState.combination != finalCombination) { //enquanto não atingir a combinação final...
-        let children = getValidPossibilities(rootState) //coletar novas combinações validas
-        for (let child of children) { //para cada combinação, cria um novo nodo e adiciona-o como filho do nodo raiz
-            if (rootState.boatSide == 'left') {
-                rootState.children.push(createNode(child, 'right'))
-            } else {
-                rootState.children.push(createNode(child, 'left'))
-            }
+
+    //enquanto não atingir a combinação final...
+    if (isDifferent(rootState.combination, finalCombination)) { 
+        //coletar novas combinações validas
+        let children = getValidPossibilities(rootState) 
+
+        //para cada combinação, cria um novo nodo e adiciona-o como filho do nodo raiz
+        for (let child of children) { 
+            rootState.children.push(createNode(child))
         }
 
-        // Calcular distancia distancias
-        // Formula: distancia entre o ponto atual e o próximo + distancia do próximo ponto até o destino
         let distances = []
         for (let child of rootState.children) {
-            let distanceCurrentNext = hammingDistance(rootState.combination, child.combination)
-            let distanceNextFinal = hammingDistance(child.combination, finalCombination)
+            let distanceCurrentNext = decimalDistance(rootState.combination, child.combination)
+            let distanceNextFinal = decimalDistance(child.combination, finalCombination)
             let distanceTotal = distanceCurrentNext + distanceNextFinal
             distances.push(distanceTotal)
         }
@@ -112,145 +130,15 @@ function createSearchATree(rootState) {
         let min = Math.min(...distances)
         let bestIndex = distances.indexOf(min)
 
-        rootState = createSearchATree(rootState.children[bestIndex])
+        //recursividade usando o melhor nodo filho
+        let newRoot = rootState.children[bestIndex]
+        combinationHistory.push(newRoot.combination)
+        rootState.children.push(createSearchATree(newRoot))
     } 
 
     return rootState
 }
 
-// console.log(binary.decimal(1010))
-root = createSearchATree(root);
-console.log(root)
-
-
-
-
-//createNode(currentState.combination, currentState.boatSide)
-
-
-
-// function checkMoreCannibals(side){
-//     if(side[0] < side[1]) return true
-// }
-
-// createPoints(currentState)
-
-// const inicio = {
-//     children: [],
-//     distanceInicial: 0,
-//     state: [3,3,1,0,0,0]
-//    }
-   
-//    const final = {
-//     parent: null,
-//     children: [],
-//     state: [0,0,0,3,3,1]
-//    }
-   
-//    const possibleActions = [
-//      [1,1],
-//      [0,1],
-//      [1,0],
-//      [2,0],
-//      [0,2],
-//    ]
-   
-//    let visitedNode = []
-//    let result
-//    let prof = 0
-   
-//    function transform(e){
-//      if(e == 0){
-//        return '000'
-//      }else if(e == 1) {
-//        return '001'
-//      }else if(e == 2) {
-//        return '011'
-//      }else if(e == 3) {
-//        return '111'
-//      }
-//    }
-   
-//    function hammingDistance(a, b) {
-//      let distance = 0
-   
-//      const newA = `${transform(a[0]) + transform(a[1]) + transform(a[3]) + transform(a[4])}`
-//      const newB = `${transform(b[0]) + transform(b[1]) + transform(b[3]) + transform(b[4])}`
-   
-//      for (let i = 0; i < newA.length; i += 1) {
-//        if (newA[i] !== newB[i]) {
-//          distance += 1
-//        }
-//      }
-    
-//      return distance;
-//    }
-   
-//    function validState(currentState){ 
-//      if(visitedNode.includes(currentState.toString())) return false
-   
-//      if((currentState[0] < currentState[1] && currentState[0] != 0) || (currentState[3] < currentState[4] && currentState[3] != 0)) return false
-   
-//      return true
-//    }
-   
-//    function nextState(currentState, next){
-//      const newState = (currentState.state[2] == 1) ? [ //Se o barco estar a esquerda (representado pelo 1), então cria 
-//            currentState.state[0] - next[0],            //um novo array de missionarios-canibais usando cada combinacao daquele array de possibilidades
-//            currentState.state[1] - next[1],            // Nesta primeira condicional, está removendo as pessoas da esquerda e jogando para a direita
-//            0, 
-//            currentState.state[3] + next[0], 
-//            currentState.state[4] + next[1], 
-//            1
-//          ] 
-//          :  [
-//              currentState.state[0] + next[0], 
-//              currentState.state[1] + next[1], 
-//              1, 
-//              currentState.state[3] - next[0], 
-//              currentState.state[4] - next[1], 
-//              0
-//          ]
-    
-//      const inicial = currentState.distanceInicial + hammingDistance(currentState.state, newState) //
-//      const nextNode = {
-//        parent: currentState,
-//        distanceInicial: inicial,
-//        totalDistance: inicial + hammingDistance(newState, final.state),
-//        state: newState,
-//        children: []
-//      }
-   
-//      if(validState(nextNode.state)) currentState.children.push(nextNode)
-//      return currentState 
-//    }
-   
-//    function print(finalState){
-//      result = `${finalState.state[0]}m ${finalState.state[1]}c ${finalState.state[2] == 1 ? ' \uD83D\uDEF6....... ' : ' .......\uD83D\uDEF6 '} ${finalState.state[3]}m ${finalState.state[4]}c \n` + (result || '')
-   
-//      if(!finalState.parent) return console.log(`Resultado da busca (profundidade ${prof}) \n\n` + result)
-     
-//      prof++
-//      print(finalState.parent)
-//    }
-   
-//    function createNode(node){
-//      let actions = node; //copia do estado inicial
-   
-//      possibleActions.forEach(p => {
-//        actions = nextState(actions, p)
-//      })
-   
-//      const next = actions.children.reduce((a, b) => {
-//        if(b.totalDistance <= a.totalDistance) a = b
-//        return a
-//      })
-   
-//      visitedNode.push(next.state.toString())
-   
-//      if(next.state.toString() == final.state.toString()) return print(next)
-    
-//      return createNode(next)
-//    }
-   
-//    createNode(inicio)
+let root = createSearchATree(null);
+//console.log(root)
+console.log(combinationHistory)
