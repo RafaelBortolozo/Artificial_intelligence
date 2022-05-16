495c7c9f356de139a23dd2c7e78c4fa22757d671
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-05-16 11:16:10 -0300

    Update cargaAviao.py

--- AG/cargaAviao.py
+++ AG/cargaAviao.py
@@ -14,7 +14,7 @@ import os
 # gerar 12 valores aleatórios, cada compartimento recebe partes das 4 cargas
 def generate_(random, args):
     size = args.get('num_inputs', 12)
-    return [random.randint(0, 23000) for i in range(size)]
+    return [random.randint(0, 16000) for i in range(size)]
  
 # Avalia a solução com a função fitness
 def evaluate_(candidates, args):
@@ -25,22 +25,61 @@ def evaluate_(candidates, args):
  
 # Calculo do fitness da solução informada
 def perform_fitness(cs):
+    totalCargas = 0
     for i, weight in enumerate(cs):
         cs[i] = np.round(cs[i])
+        totalCargas += cs[i]
     
     #         | Dianteiro | Central | Traseiro |
-    # Carga 1 |   cs[0]   |  cs[4]  |  cs[8]   |
-    # Carga 2 |   cs[1]   |  cs[5]  |  cs[9]   |
-    # Carga 3 |   cs[2]   |  cs[6]  |  cs[10]  |
-    # Carga 4 |   cs[3]   |  cs[7]  |  cs[11]  |
+    # Carga 1 |   cs[0]   |  cs[1]  |  cs[2]   |
+    # Carga 2 |   cs[3]   |  cs[4]  |  cs[5]   |
+    # Carga 3 |   cs[6]   |  cs[7]  |  cs[8]   |
+    # Carga 4 |   cs[9]   |  cs[10] |  cs[11]  |
 
-    fit = float((5*L + 4.5*S) / 7375)
-    h1 = np.maximum(0, float(((6*L+5*S)/100)-60)) / 15
-    h2 = np.maximum(0, float(((10*L+20*S)-15000))) / 3750
-    h3 = np.maximum(0, float(L-800)) / 200
-    h4 = np.maximum(0, float(S-750)) / 187.5
- 
-    fit = fit - (h1 + h2 + h3 + h4)
+    # CALCULO DO FIT, MULTIPLICA O PESO(kg) PELO PREÇO, SOMA TUDO E DIVIDE PELO ???? 
+    vpc1 = 0.31  # Valor do peso na carga 1 (R$ 310/t)
+    vpc2 = 0.38  # Valor do peso na carga 2 (R$ 380/t)
+    vpc3 = 0.35  # Valor do peso na carga 3 (R$ 350/t)
+    vpc4 = 0.285 # Valor do peso na carga 4 (R$ 285/t)
+    
+    # TOTAL DA CARGA x DENTRO DO AVIAO
+    somaCarga1 = (cs[0] + cs[1] + cs[2])
+    somaCarga2 = (cs[3] + cs[4] + cs[5])
+    somaCarga3 = (cs[6] + cs[7] + cs[8])
+    somaCarga4 = (cs[9] + cs[10] + cs[11])
+
+    # TOTAL DE PESO EM CADA COMPARTIMENTO DO AVIAO
+    somaDianteira = (cs[0] + cs[3] + cs[6] + cs[9])
+    somaCentral = (cs[1] + cs[4] + cs[7] + cs[10])
+    somaTraseira = (cs[2] + cs[5] + cs[8] + cs[11])
+
+    fit = float((somaCarga1*vpc1 + somaCarga2*vpc2 + somaCarga3*vpc3 + somaCarga4*vpc4) / 12350)
+
+    # PENALIZAÇÕES
+    qtdH = 13
+
+    # PENALINAZAÇÃO QUANTO AO PESO DAS CARGAS
+    h1 = np.maximum(0, float(somaCarga1 - 18000)) / (18000/qtdH)
+    h2 = np.maximum(0, float(somaCarga2 - 15000)) / (15000/qtdH)
+    h3 = np.maximum(0, float(somaCarga3 - 23000)) / (23000/qtdH)
+    h4 = np.maximum(0, float(somaCarga4 - 12000)) / (12000/qtdH)
+    
+    # PENALIZAÇÃO QUANTO AO PESO DOS COMPARTIMENTOS DO AVIAO
+    h5 = np.maximum(0, float(somaDianteira - 10000)) / (10000/qtdH)
+    h6 = np.maximum(0, float(somaCentral - 16000)) / (16000/qtdH)
+    h7 = np.maximum(0, float(somaTraseira - 8000)) / (8000/qtdH)
+
+    # PENALIZAÇÕES QUANTO AO VOLUME DOS COMPARTIMENTOS DO AVIAO
+    h8 = np.maximum(0, float(cs[0]*0.48 + cs[3]*0.65 + cs[6]*0.58 + cs[9]*0.39)-6800) / (6800/qtdH)
+    h9 = np.maximum(0, float(cs[1]*0.48 + cs[4]*0.65 + cs[7]*0.58 + cs[10]*0.39)-8700) / (8700/qtdH)
+    h10 = np.maximum(0, float(cs[2]*0.48 + cs[5]*0.65 + cs[8]*0.58 + cs[11]*0.39)-5300) / (5300/qtdH)
+
+    # PENALIZAÇÕES QUANTO A PROPORÇÃO DE CADA COMPARTIMENTO DO AVIAO
+    h11 = np.maximum(0, float(((somaDianteira / totalCargas) - 0.29411764706))) / (0.29411764706/qtdH)
+    h12 = np.maximum(0, float(((somaCentral / totalCargas) - 0.47058823529))) / (0.47058823529/qtdH)
+    h13 = np.maximum(0, float(((somaTraseira / totalCargas) - 0.23529411765))) / (0.23529411765/qtdH)
+
+    fit = fit-(h1+h2+h3+h4+h5+h6+h7+h8+h9+h10+h11+h12+h13)
     return fit
  
 # Avaliação final do melhor indivíduo(objetivo)
